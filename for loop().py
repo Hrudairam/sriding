1361f017deb70a4114435d0ddde6cc6daf243992
Hrudai Ram <ramhrudai@gmail.com>
2026-02-02 22:22:59 +0530

    Add files via upload

---
+++ for loop().py
@@ -0,0 +1,85 @@
+#for,while,range,break,continue,pass
+#for loop()
+'''a=[10,20,30,40,50,60]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,40,50,60]
+for i in a:
+    print(a)'''
+
+'''a=[10,20,30,40,50,60]
+for i in a:
+    print(i,end=",")'''
+
+'''a=[10,20,30,40,50,60]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a={10,20,30,40,50,60}
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a={"name":"ram","year":2026,"month":1}
+for i in a.items():
+    print(i)'''
+ 
+'''a={"name":"ram","year":2026,"month":1}
+for i in a.items():
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''fruits=["apple","bananna","grapes"]
+for i in fruits:
+    print(i,end=" ")
+    print(type(fruits))
+    print(type(i))'''
+
+'''a = ["python", "java", "html"]
+b = [i.capitalize()]
+for i in a:
+     print(b)'''
+#while loop
+'''a=10
+while a>1:
+    print(a)'''
+    
+'''a=10
+while a>1:
+    print(a)
+    a=a-1'''
+'''a=10
+while a>1:
+    a=a-1
+    print(a)'''
+
+'''a=10
+while a>1:
+    print(a)
+    a=a-1'''
+'''a=10
+while a>1:
+    print(a)
+    a+=1'''
+
+
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
